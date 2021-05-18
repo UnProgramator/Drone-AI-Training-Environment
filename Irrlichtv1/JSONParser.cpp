e2767963b64a2835584ab263258b3ee2a112a067
Alex Pescaru <apescaru11@gmail.com>
2021-05-18 15:27:38 +0300

    added drone parse into Base_JSONParser

--- Irrlichtv1/JSONParser.cpp
+++ Irrlichtv1/JSONParser.cpp
@@ -1,1 +1,34 @@
 #include "JSONParser.h"
+#include "ObjectFactory.h"
+#include <nlohmann/json.hpp>
+#include <fstream>
+#include <iostream>
+
+using Json = nlohmann::json;
+
+Base_JSONParser::Base_JSONParser(DefaultObjectFactory* object_factory):
+	object_factory{object_factory}
+{
+}
+
+Base_JSONParser::~Base_JSONParser()
+{
+}
+
+Drone* Base_JSONParser::parseDrone(const std::string& filePath)
+{
+	Drone* drone;
+	Json json; 
+	{
+		std::ifstream fin(filePath);
+		fin >> json;
+		fin.close();
+	}
+	drone = object_factory->getDrone(json);
+	for (auto& sensor : json["sensors"]) {
+		std::string name = sensor["name"].get<std::string>();
+		auto& atribs = sensor["attributes"];
+		drone->add_sensor(object_factory->getSensor(name, atribs));
+	}
+	return drone;
+}
--- Irrlichtv1/JSONParser.h
+++ Irrlichtv1/JSONParser.h
@@ -1,5 +1,26 @@
 #pragma once
+
+#include "Drone.h"
+#include "ObjectFactory.h"
+#include <memory>
+
 class Base_JSONParser
 {
+private:
+	DefaultObjectFactory* object_factory;
+public:
+	//constructors and destructors
+	Base_JSONParser(DefaultObjectFactory* object_factory);
+	virtual ~Base_JSONParser();
+
+	Base_JSONParser(const Base_JSONParser&) = delete;
+	Base_JSONParser(Base_JSONParser&&) = delete;
+
+	Base_JSONParser& operator=(const Base_JSONParser&) = delete;
+	Base_JSONParser& operator=(Base_JSONParser&&) = delete;
+
+	//files parsers
+
+	virtual Drone* parseDrone(const std::string& filePath);
 };
 
